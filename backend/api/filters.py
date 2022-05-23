adcb53197bcfe5b597483da17696c7effc58006f
Иван <viserditech@yandex.ru>
2022-05-23 18:42:57 +0300

    fix filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,14 +1,27 @@
+from distutils.util import strtobool
+
 import django_filters
 from rest_framework import filters
 
 from recipes.models import Favorite, Recipe, Shoppingcart
 
+CHOICES = (
+    ('0', 'False'),
+    ('1', 'True')
+)
+
 
 class RecipeFilter(django_filters.FilterSet):
     author = django_filters.CharFilter(field_name='author__id')
     tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = django_filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = django_filters.BooleanFilter(
+    is_favorited = django_filters.TypedChoiceFilter(
+        choices=CHOICES,
+        coerce=strtobool,
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = django_filters.TypedChoiceFilter(
+        choices=CHOICES,
+        coerce=strtobool,
         method='get_is_in_shopping_cart'
     )
 
@@ -17,8 +30,8 @@ class RecipeFilter(django_filters.FilterSet):
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def get_is_favorited(self, queryset, name, value):
-        if not value:
-            return queryset
+        if not value :
+            return queryset 
         favorites = Favorite.objects.filter(user=self.request.user)
         return queryset.filter(
             pk__in=(favorite.recipe.pk for favorite in favorites)
